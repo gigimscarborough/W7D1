2c225d9a02cf9fe0684248c0380ae55e81f1ad25
gscarborough <gigimscarborough@gmail.com>
2020-12-07 14:29:05 -0500

    started user model

---
+++ 99Cats_Part2/app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+    validates :user_name, :password_digest, :session_token, presence: true
+    validates :user_name, :session_token, uniqueness: true
+    validates :password, length: {minimum: 6} allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        
+    end
+
+    def reset_session_token!
+        self.update!(session_token: self.class.generate_session_token)
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(64)
+    end
+
+    def is_password?(password)
+        bcrypt_password = BCrypt::Password.new(self.password_digest)
+        bcrypt_password.is_password?(password)
+    end
+
+
+
+
+end
